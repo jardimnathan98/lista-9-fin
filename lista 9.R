e78ae6d4de19df586fa71ad3e5dd48f6c8280320
jardimnathan98 <53458619+jardimnathan98@users.noreply.github.com>
2022-11-02 10:53:09 -0300

    Update lista 9.R

--- lista 9.R
+++ lista 9.R
@@ -4,22 +4,50 @@ library(dplyr)
 library(PerformanceAnalytics)
 library(ggplot2)
 library(tidyverse)
-library(writexl)
-library(readxl)
-library(vrtest)
-library(GetTddata)
-library(xlsx)
-# define os ativos que ir?o ser coletados
-# define a data de in?cio da coleta
+#library(writexl)
+#library(readxl)
+#library(vrtest)
+#library(GetTddata)
+#library(xlsx)
+# define os ativos que irao ser coletados
+# define a data de inicio da coleta
 
 start <- "2012-06-30"
 getSymbols("^BVSP",
            warnings = FALSE,
            from = start,
            src = "yahoo")
+
 #fazer retornos trimestrais aqui
+bvsp_adj<-as.data.frame(BVSP$BVSP.Adjusted)
+bvsp_adj_returns<-log()
 
+
+bvsp_returns_quart <- Ad(BVSP) %>%
+  to.quarterly(indexAt = "lastof",
+               OHLC = FALSE) %>%
+  Return.calculate(method = "log") %>%
+  na.omit() %>%
+  `colnames<-`("ibovespa")
+
+bvsp_returns_quart<-as.matrix(bvsp_returns_quart)
+load("/home/nathan/Downloads/mestrado/lista 9/ipca_quarter.Rdata")
+
+bvsp_returns_quart_real<-bvsp_returns_quart[1:(nrow(bvsp_returns_quart)- 3),1]/(1 + as.numeric(ipca_quarter[75:(nrow(ipca_quarter)-1),2])/100)
+bvsp_returns_quart_real<-as.matrix(bvsp_returns_quart_real)
 #
-
-di<-read.csv("C:/Users/Nathan/Downloads/livros economia/financas/finanças puc/lista 9/di futuro.csv")
 #pegar di 1 mes a cada 3 meses para livre de risco
+di<-read.csv("/home/nathan/Downloads/mestrado/lista 9/di futuro.csv")
+di_1m<-di[,2:3]
+di_1m<-di_1m<-as.matrix(rev(di_1m[1:119,]))#botar na mesma data dos demais
+
+di_quarter<-cbind(bvsp_returns_quart_real,0)
+j<-1
+for(i in 0:(nrow(di_1m)/3)){
+  di_quarter[j,]<-di_1m[nrow(di_1m)-3*i,]
+j<-j+1
+  }
+
+
+#montar consumo per capita
+
